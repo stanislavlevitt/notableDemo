33c161fbb18058eb95c8c05d2ea48f312e3ff823
Stanislav Levitt <stanislavlevitt@yahoo.com>
2022-11-18 15:13:04 -0500

    get route for all physicians

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/', require('./hello'))
+router.use('/physicians', require('./physicians'))
+// router.use('/appointments', require('./appointments'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
